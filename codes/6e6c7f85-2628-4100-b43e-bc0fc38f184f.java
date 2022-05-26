afa81594ca4996d33e231a7fa429dc2445e0864a
jaagrav <jaagravseal03@gmail.com>
2022-05-26 22:29:22 +0530

    Removed codes and classes temp directories

--- codes/6e6c7f85-2628-4100-b43e-bc0fc38f184f.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-class program{
-public static void main(String [] args){  
-Scanner in =new Scanner(System.in);
-System.out.println(in.nextLine());
-System.out.println(in.nextLine()); 
-} 
-}
--- codes/992aeba4-50e1-4688-826f-4eee349559ff.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-class program{
-public static void main(String [] args){  
-Scanner in =new Scanner(System.in);
-System.out.println(in.nextLine() + "\n" + in.nextLine()); 
-} 
-}
